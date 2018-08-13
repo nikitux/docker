0ec6097e571c64cf6efd4217712a65687c0c601f
nikitux <nikitux@gmail.com>
2018-08-13 15:17:15 -0300

    Added s3 upload features

--- cloudflare-backup/entrypoint.sh
+++ cloudflare-backup/entrypoint.sh
@@ -2,5 +2,11 @@
 
 touch /bkp/zones.txt
 
+if ! [ -z "$AWS_S3_ENABLE" ] && [ "$AWS_S3_ENABLE" = "true" ]; then 
+  
+  sed -i "/touch \/bkp\/zones.txt/a aws s3 sync \/bkp\/ s3://$AWS_BUCKET_NAME " /etc/logrotate.d/cfbkp
+fi
+
+
 /usr/sbin/crond -f
 
